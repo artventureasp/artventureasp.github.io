9b6205abb6ccd768e6b6a8f9b8ec54a2919ba3d0
Vineet <erasalav@gmail.com>
2024-07-09 21:11:42 -0400

    uploaded database schema sql file

---
+++ db_schema.sql
@@ -0,0 +1,10 @@
+-- This makes sure that foreign_key constraints are observed and that errors will be thrown for violations
+PRAGMA foreign_keys=ON;
+
+BEGIN TRANSACTION;
+
+-- SQL commands here (remember, there are syntactical differences with SQLite)
+
+
+
+COMMIT;
