65a251838a6463d828ff90bed914a60f2909b710
Anton Pashkovskyi <anton@globalbit.co.il>
2024-07-22 17:40:42 +0200

    add mongodb connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MONGODB_URI = 'mongodb+srv://antonpash:9HkLmDhekt6YRCaF@cluster0.wpcuwpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
