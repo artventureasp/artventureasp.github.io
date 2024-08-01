a31d92d3addc1e8708bce39374e25417499172ad
Vineet <erasalav@gmail.com>
2024-08-01 19:59:39 -0400

    added rudimentary navbar and footer and included links in the routing engine. styling and frontend untouched.

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+      <p>© 2024 Artventure Project Group</p>
+      <router-link to="/">Professional Help</router-link>
+    </footer>
+  </template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+/* Add styling here, adding tags above as required*/
+</style>
---
+++ frontend/src/views/CreatePost.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Create a Post</h1>
+</template>
