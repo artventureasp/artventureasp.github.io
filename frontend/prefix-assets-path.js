f4a71b1edbc21ce74d807d7068efaa54da82b917
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2024-08-06 13:18:42 +0200

    Delete frontend/prefix-assets-path.js

--- frontend/prefix-assets-path.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-let content = fs.readFileSync('./dist/index.html').toString('utf-8');
-content = content.replace(/\/assets/g, '/ArtVenture-ASP/assets');
-fs.writeFileSync('./dist/index.html', content);
