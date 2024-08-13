96044e9510be954086de7d22654dc30d210de584
Anton Pashkovskyi <anton@globalbit.co.il>
2024-08-13 18:36:05 +0200

    change firebase PK extraction fron env vars

--- api/src/services/firebase.service.ts
+++ api/src/services/firebase.service.ts
@@ -6,11 +6,12 @@ import { getStorage } from "firebase-admin/storage";
 @Injectable()
 export class FirebaseService {
   constructor(private configService: ConfigService) {
+    const { privateKey } = JSON.parse(configService.get<string>('FIREBASE_PRIVATE_KEY'));
     initializeApp({
       credential: cert({
         projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
         clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-        privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY'),
+        privateKey,
       }),
       storageBucket: configService.get<string>('FIREBASE_STORAGE_BUCKET'),
     });
