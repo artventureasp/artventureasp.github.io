9597dc1b740e2b6a2a140b06f7037d05fcc7d456
Anton Pashkovskyi <anton@globalbit.co.il>
2024-08-29 19:05:26 +0200

    add home page UI

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,5 +2,76 @@
 </script>
 
 <template>
-  <h1>Home page</h1>
+  <div class="container py-5">
+    <div class="hero mb-3">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-12 col-md-auto">
+          <div class="text-center">
+            <img class="mb-3" src="@/assets/images/renovation-1262389_1280.png" alt="cat painting on the wall">
+          </div>
+        </div>
+        <div class="col">
+          <h1 class="fs-3 text-center mb-4">Come To ArtVenture & Express yourself in Arts!</h1>
+          <p class="text-center">An Art Therapy Themed Social Media Platform, in which you can express your feelings and stories that can’t be described with words but arts.</p>
+          <p class="text-center">When words are difficult to be made, let yourself find an art you can relate to or make an art for only yourself and your emotions and share your stories with us!</p>
+        </div>
+      </div>
+    </div>
+    <div class="about">
+      <h3 class="text-center mb-4">About ArtVenture</h3>
+      <div class="row align-items-center mb-4">
+        <div class="col-12 col-md-auto text-center mb-2">
+          <img src="@/assets/images/mountains-7409870_1280 1.png" alt="abstract mountains">
+        </div>
+        <div class="col">
+          <p>Our Art Venture web app is a social media platform in which you can share the arts you like or you make to express your feelings or just to show the arts...for whatever reasons.. this place is a place where you can feel the power of arts and the beauty of arts...</p>
+        </div>
+      </div>
+      <div class="row align-items-center mb-4">
+        <div class="col-12 d-md-none text-center mb-2">
+          <img src="@/assets/images/cat-6138366_1280 1.png" alt="cat with light inside">
+        </div>
+        <div class="col">
+          <p>Express your emotions and the stories you would like to share in the forms of whatever arts you like or make...Would it be a song? Would it be a painting, a picture, a simple drawing or a doodle? Would it be a video where you find yourself connect with the music of it or the show of it? Whatever they are...you can express all the arts you like or you make as much as your heart allows you to on our ArtVenture...</p>
+        </div>
+        <div class="d-none d-md-block col-md-auto text-center">
+          <img src="@/assets/images/cat-6138366_1280 1.png" alt="cat with light inside">
+        </div>
+      </div>
+      <div class="row align-items-center mb-4">
+        <div class="col-12 col-md-auto text-center mb-2">
+          <img src="@/assets/images/bird-616803_1280 1.png" alt="singing bird">
+        </div>
+        <div class="col">
+          <p>Set yourself free in the world of arts on ArtVenture. Deep dive into the world of your emotions and your life and give a moment for yourself and your feelings by interacting with our fellow ArtVenturers and sharing the arts you can relate to or the arts which tell the stories for you on ArtVenture.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.hero img {
+  width: 100%;
+  max-width: 400px;
+}
+
+.hero h1 {
+  color: #18623F;
+}
+
+.about {
+  width: 100%;
+}
+
+.about img {
+  max-width: 150px;
+}
+
+@media screen and (min-width: 1200px) {
+  .about {
+    width: 75%;
+    margin: 0 auto;
+  }
+}
+</style>
